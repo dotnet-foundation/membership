8008e19927762c270233f162504c7faa782cf054
Oren Novotny <oren@novotny.org>
2019-01-04 16:36:22 -0500

    Allow anon

--- Membership/Controllers/HomeController.cs
+++ Membership/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 namespace Membership.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class HomeController : Controller
     {
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var token = await HttpContext.GetTokenAsync("access_token");
+            //var token = await HttpContext.GetTokenAsync("access_token");
             
             return View();
         }
