48cffa473d0e4e587eba7dd8796c574f77015da7
Oren Novotny <oren@novotny.org>
2019-01-09 11:47:25 -0500

    Enagle profile edit for individuals

--- Membership/Controllers/ProfileController.cs
+++ Membership/Controllers/ProfileController.cs
@@ -30,8 +30,42 @@ namespace Membership.Controllers
         {
             var member = await _usersService.GetMe();
 
-            
+            var model = new UpdateMemberModel
+            {
+                Id = member.Id,
+                GivenName = member.GivenName,
+                Surname = member.Surname,
+                DisplayName = member.DisplayName,
+                GitHubId = member.GitHubId,
+                Expiration = member.Expiration,
+                IsActive = member.IsActive
+            };
+
             return View(member);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, UpdateMemberModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                var oid = User.FindFirst("oid").Value; // ignore any params and use the claims
+
+                await _usersService.UpdateMemberAsync(oid, model.DisplayName, null, model.GivenName, model.Surname, model.GitHubId, null);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ModelState.TryAddModelError("", e.Message);
+                return View(model);
+            }
         }
     }
 }
