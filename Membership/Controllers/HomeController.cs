fdfc2459f6df9ee8c213a58018504b005b6294f2
Jon Galloway <jongalloway@gmail.com>
2019-02-13 15:24:56 -0800

    Updated cookie consent and privacy policy links and info

--- Membership/Controllers/HomeController.cs
+++ Membership/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Membership.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
